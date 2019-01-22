60145a9a92deb5405e170d2b16ab52eae2ae25cb
czops <37029225+czops@users.noreply.github.com>
2019-01-22 17:58:50 -0600

    for some reason adding a new button prevents ALL buttons from working

--- assets/javascript/JavaScript.js
+++ assets/javascript/JavaScript.js
@@ -2,13 +2,13 @@
 
 //build the query
 
-var topics = ["cats", "puppies", "slav", "boxing", "soccer", "climbing", "china", "zen", "kittens", "skiing"];
+var topics = ["cats", "puppies", "slap", "boxing", "soccer", "climbing", "china", "zen", "kittens", "skiing"];
 
 function appendButtons() {
   for (i = 0; i < topics.length; i++) {
 
     var NewButton = $("<button>").attr("class", "btn btn-primary").html(topics[i]).val(topics[i]);
-    $("#button-div").prepend(NewButton);
+    $("#button-div").append(NewButton);
 
   }
 }
@@ -17,19 +17,26 @@ appendButtons();
 
 //this function should get called when you submit the form
 function createNewButton(){
-  var newButton = $("#buttonInput", text);
+  var newButton = $("#GifButtonInput").val();
   topics.push(newButton);
+  console.log(topics);
+
   $("#button-div").empty();
+
   for (i = 0; i < topics.length; i++) {
 
     var NewButton = $("<button>").attr("class", "btn btn-primary").html(topics[i]).val(topics[i]);
-    $("#button-div").prepend(NewButton);
+    $("#button-div").append(NewButton);
 
   }
 };
 
+$("#GifButton").on("click", function(event) {
+  createNewButton();
+});
 
 
+//currently not using this
 function buildQueryURL() {
   // queryURL is the url we'll use to query the API
   var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=";
@@ -66,16 +73,17 @@ function buildQueryURL() {
 };
 
 
-$("button").on("click", function (event) {
+
+$(".btn-primary").on("click", function (event) {
   //take the html and set it to the search term for the JSON call
-  var animal = $(this).attr("value");
+  var gif = $(this).attr("value");
 
   var queryParams = { "apikey": "hSQi8HlYRYNBHYHM3lWKVTWBodAgy1E4" };
 
   // Constructing a queryURL using the animal name
   //here i plugged in my API key from Giphy
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    animal + "&api_key=" + queryParams.apikey + "&limit=10";
+    gif + "&api_key=" + queryParams.apikey + "&limit=10";
 
   // Performing an AJAX request with the queryURL
   $.ajax({
@@ -94,22 +102,41 @@ $("button").on("click", function (event) {
       for (var i = 0; i < results.length; i++) {
 
         // Creating and storing a div tag
-        var animalDiv = $("#gif-div");
+        var gifDiv = $("#gif-div");
 
         // Creating a paragraph tag with the result item's rating
         var p = $("<p>").text("Rating: " + results[i].rating);
 
         // Creating and storing an image tag
         var animalImage = $("<img>");
+        animalImage.attr("class", "NewGif")
+
         // Setting the src attribute of the image to a property pulled off the result item
         animalImage.attr("src", results[i].images.fixed_height.url);
 
         // Appending the paragraph and image tag to the animalDiv
-        animalDiv.append(p);
-        animalDiv.append(animalImage);
+        gifDiv.append(p);
+        gifDiv.append(animalImage);
 
         // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-        $("#gifs-appear-here").prepend(animalDiv);
+        $("#gifs-appear-here").prepend(gifDiv);
       }
     });
+});
+
+
+//trying to get the gifs to load paused
+$(".gif").on("click", function() {
+  // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+  var state = $(this).attr("data-state");
+  // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+  // Then, set the image's data-state to animate
+  // Else set src to the data-still value
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
 });
